403b4f2dbffa275aa7fca12c6768c200c23c4d31
Emmzy17 <emmanueladjerry@gmail.com>
2022-10-28 13:50:43 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from urllib import response
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class response_schema(BaseModel):
+    slackUsername : str
+    backend: bool
+    age : int
+    bio: str
+
+
+app.get('/', response_model=response_schema)
+def request():
+    return {"slackUsername": "shakzy", "backend": True, "age": 19, "bio": "I am a Software Engineer, with a love for Problem Solving and Building Thngs"}
